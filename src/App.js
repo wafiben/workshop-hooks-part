006e5ec8248d48d46325f1892bfd652f7a2b3545
wafiben <wafibenjeddou@gmail.com>
2022-08-02 21:02:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [students, setStudents] = useState(["souheil"]);
+  const [student, setStudent] = useState("");
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecremt = () => {
+    if (count > 0) setCount(count - 1);
+  };
+  const handleChange = (event) => {
+    setStudent(event.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStudents([...students, student]);
+    setStudent("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleIncrement}>+</button>
+      <h1>{count}</h1>
+      <button onClick={handleDecremt}>-</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={student} />
+        <button type="submit">Submit</button>
+      </form>
+      {students.map((elt, index) => (
+        <h1 key={index}>{elt}</h1>
+      ))}
     </div>
   );
 }
